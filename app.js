9bb875f4da236b6a0bf62bea1874f4cd1bd232a7
Paul Bailey <paul@neutrondrive.com>
2017-06-15 09:38:59 -0500

    heroku setup

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ app.get('/', function (request, response) {
   response.render('home.hbs', {});
 });
 
-app.get('/api', cache('5 minutes'), function (request, response) {
+app.get('/api', cache('5 minutes'), function (request, response, next) {
   console.log('Generating new response');
   axios.get('https://api.darksky.net/forecast/b651ed0ee8d5810d8dcea9f8151817b6/37.8267,-122.4233').then(function (r) {
       response.json(r.data);
-  });
+  })
+  .catch(next);
 });
 
 var PORT = process.env.PORT || 8000;
